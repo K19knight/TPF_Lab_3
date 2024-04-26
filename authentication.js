f2aa2e1fa44884a6940f1fbbd3d6e6b91f334f61
Konrad Blicharz <konradknight19@gmail.com>
2024-04-26 21:18:37 +0200

    TPF lab 4 task done

--- authentication.js
+++ authentication.js
@@ -38,6 +38,7 @@ const userSignOut = async () => {
     }).catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
+        alert(errorMessage +  errorCode)
     })
 }
 
